2585fe53385e662475e8834ff1231d1ca6d63286
Indra Ikhwal <indra@iglobal.co.id>
2022-10-15 21:25:43 +0700

    Update controller function

--- models/menuModel.go
+++ models/menuModel.go
@@ -10,8 +10,8 @@ type Menu struct {
 	ID         primitive.ObjectID `bson:"_id"`
 	Name       string             `json:"name" validate:"required,min=2,max=100"`
 	Category   string             `json:"category" validate:"required"`
-	Start_date string             `json:"start_date"`
-	End_date   string             `json:"end_date"`
+	Start_date *time.Time         `json:"start_date"`
+	End_date   *time.Time         `json:"end_date"`
 	Created_at time.Time          `json:"created_at"`
 	Updated_at time.Time          `json:"Updated_at"`
 	Menu_id    string             `json:"menu_id"`
